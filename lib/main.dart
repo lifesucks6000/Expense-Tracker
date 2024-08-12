61305ba4abcb3dd3501734d78c14d69246934a4f
Naveen Singh <sachinsinghmaths@gmail.com>
2024-08-12 22:52:36 +0530

    disabling the debug banner on the corner of the app

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Expense Tracker',
       theme: _buildLightTheme(),
       darkTheme: _buildDarkTheme(),
